774c19ecebf44fcecaef4575473099321cf04037
MassiGy <ghernaoutmassi@gmail.com>
2021-04-10 11:30:01 +0100

    adding the license terms & secure cookies option

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const sessionConfig = {
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        // secure: true,
+        secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
