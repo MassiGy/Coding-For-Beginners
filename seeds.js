2aaed8635ee7219cbba5374fc9c1832105930cc5
MassiGy <ghernaoutmassi@gmail.com>
2023-05-23 12:15:58 +0200

    +master: commit to trigger automatic deployement

--- seeds.js
+++ seeds.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Feedback = require('./public/Modals/feedbacks')
 const Signup = require('./public/Modals/signup')
 
+
+// to seed the local databse
 mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connection enabled at port 27017')
@@ -10,11 +12,6 @@ mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useU
         console.log(ERROR)
     })
 
-
-
-
-// let seedednewUser = new Signup({ "postedFeedbacks": [], "username": "MassiGy", "email": "ghernaoutmassi@gmail.com", "password": "massigy", })
-// let seededfeedback = new Feedback({ username: 'MassiGy', email: 'ghernaoutmassi@gmail.com', password: 'massigy', feedbackText: 'mass', userSatisfied: 'Yes', websiteProblem: 'The content was general' })
 
 let formater = async(modal) => await modal.deleteMany({});
 formater(Signup);
